b44dc6b05acf03a358914d91c19f8ac514651866
Alexander Mendez <bluespace@gmail.com>
2022-09-19 13:55:55 -0700

    adding setup for m1

--- setup.sh
+++ setup.sh
@@ -8,4 +8,13 @@ bash miniconda.sh
 conda create --name flask flask jupyterlab
 conda activate flask
 conda install -c conda-forge opencv
+conda install pytorch torchvision torchaudio -c pytorch
+conda install matplotlib numpy pandas
+conda install av -c conda-forge
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/xamendez/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew install wget
 
+conda create --name torch 
+conda install pytorch torchvision torchaudio -c pytorch-nightly
